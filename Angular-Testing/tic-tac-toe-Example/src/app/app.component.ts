a3ca27fe0b0e347dad9972a8f9a48d035268f722
linnyHPx360 <linny1506@gmail.com>
2024-03-05 12:00:14 -0500

    Angular.js: restart v2 :(

--- Angular-Testing/tic-tac-toe-Example/src/app/app.component.ts
+++ Angular-Testing/tic-tac-toe-Example/src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';                    // default
-import { RouterOutlet } from '@angular/router';               // default
-import { SquareComponent } from "./square/square.component";  // added for the new component
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [RouterOutlet, SquareComponent]
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'tic-tac-toe-Example';
