27362ff087ab13c0f82ed45613426f0455a29e77
linnyHPx360 <linny1506@gmail.com>
2024-03-05 14:32:43 -0500

    Angular: tic-tac-toe'd

--- Angular-Testing/tic-tac-toe-Example/src/app/app.module.ts
+++ Angular-Testing/tic-tac-toe-Example/src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';   // NB: to import this automatically, you NEED to specify it in the ng new [project name] --standalone false (default for current [5Mar2024] version of AngularJS is true)
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SquareComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
