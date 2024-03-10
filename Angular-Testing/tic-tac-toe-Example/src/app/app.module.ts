4283ae3d99ede45c5b520f7edbe4634e4d7f04b0
linnyHPx360 <linny1506@gmail.com>
2024-03-10 11:27:07 -0400

    Angular: fininhsed tic-tac-toe-Example project. DO NOT PULL THIS> it probably only works on this device...changed directory structure so the files might not be linked in the correct order/place when pulled

--- Angular-Testing/tic-tac-toe-Example/src/app/app.module.ts
+++ Angular-Testing/tic-tac-toe-Example/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';   // NB: to import this automatically, you NEED to specify it in the ng new [project name] --standalone false (default for current [5Mar2024] version of AngularJS is true)
+import { NgModule, isDevMode } from '@angular/core';   // NB: to import this automatically, you NEED to specify it in the ng new [project name] --standalone false (default for current [5Mar2024] version of AngularJS is true)
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SquareComponent } from './square/square.component';
 import { BoardComponent } from './board/board.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,13 @@ import { BoardComponent } from './board/board.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
