5a974576bd5e09200dfc9a9134c23e4be2a34101
linnyHPx360 <linny1506@gmail.com>
2024-05-21 00:03:17 -0400

    Angular: putting a bookmark on radio button component, still need to figure out how to make Angular Material work with forms...

--- Angular-Testing/angular-material-playground/src/app/test-components/radio-button/radio-button.component.ts
+++ Angular-Testing/angular-material-playground/src/app/test-components/radio-button/radio-button.component.ts
@@ -15,11 +15,11 @@ export class RadioButtonComponent {
   form:FormGroup;
 
   options = [
-    {label: 'Option 1', value: 'option1'},
-    {label: 'Option 2', value: 'option2'},
-    {label: 'Option 3', value: 'option3'},
-    {label: 'Option 4', value: 'option4'},
-    {label: 'None of the Above', value: 'option5'},
+    {label: 'Option 1', index: 'option1'},
+    {label: 'Option 2', index: 'option2'},
+    {label: 'Option 3', index: 'option3'},
+    {label: 'Option 4', index: 'option4'},
+    {label: 'None of the Above', index: 'option5'},
   ]
 
   // NB: alternative way of dependency injection
@@ -31,8 +31,8 @@ export class RadioButtonComponent {
     })
   }
 
-  submit() {
-    console.log("submit: ", this.form.value);
+  poll(){
+    console.log("poll(): ", this.form.value.selectedOption);
     
   }
 }
