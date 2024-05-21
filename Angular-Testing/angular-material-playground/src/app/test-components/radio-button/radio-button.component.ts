aee244054aecfbd99d94f12aeec14ea5b5fdfcb1
linnyHPx360 <linny1506@gmail.com>
2024-05-20 21:13:21 -0400

    Angular: Vanilla Angular, made radio buttons

--- Angular-Testing/angular-material-playground/src/app/test-components/radio-button/radio-button.component.ts
+++ Angular-Testing/angular-material-playground/src/app/test-components/radio-button/radio-button.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {MatRadioModule} from '@angular/material/radio';
 
-import { FormGroup, FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
@@ -14,13 +14,20 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 export class RadioButtonComponent {
   form:FormGroup;
 
+  options = [
+    {label: 'Option 1', value: 'option1'},
+    {label: 'Option 2', value: 'option2'},
+    {label: 'Option 3', value: 'option3'},
+    {label: 'Option 4', value: 'option4'},
+    {label: 'None of the Above', value: 'option5'},
+  ]
+
+  // NB: alternative way of dependency injection
+  // fb = inject(FormBuilder);
+
   constructor(private fb:FormBuilder) {
     this.form = fb.group({
-      value1:[],
-      value2:[],
-      value3:[],
-      value4:[],
-      value5:[],
+      selectedOption: ['', Validators.required],
     })
   }
 
