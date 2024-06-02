e6c79c0e83f778722e8c1af0984b1a9bea7b55e0
linnyHPx360 <linny1506@gmail.com>
2024-06-02 19:14:53 -0400

    Angular: added another component, radio buttons :)

--- Angular-Testing/foliq-quiz/src/app/quiz/quiz-components/user-profile/user-profile-quiz/user-profile-quiz.component.ts
+++ Angular-Testing/foliq-quiz/src/app/quiz/quiz-components/user-profile/user-profile-quiz/user-profile-quiz.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HairConcernsFormComponent } from '../components/hair-concerns-form/hair-concerns-form.component';
 import { ScalpConcernsFormComponent } from '../components/scalp-concerns-form/scalp-concerns-form.component';
+import { PostWashComponent } from '../components/post-wash/post-wash.component';
 
 
 
 @Component({
   selector: 'app-user-profile-quiz',
   standalone: true,
-  imports: [HairConcernsFormComponent, ScalpConcernsFormComponent],
+  imports: [HairConcernsFormComponent, ScalpConcernsFormComponent, PostWashComponent],
   templateUrl: './user-profile-quiz.component.html',
   styleUrl: './user-profile-quiz.component.scss'
 })
