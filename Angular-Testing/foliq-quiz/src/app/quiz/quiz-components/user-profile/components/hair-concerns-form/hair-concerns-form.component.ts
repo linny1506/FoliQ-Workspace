247de734664facff9d8c7e5b27a389fc1d66d836
linnyHPx360 <linny1506@gmail.com>
2024-06-02 17:52:58 -0400

    Angular: restart :(

---
+++ Angular-Testing/foliq-quiz/src/app/quiz/quiz-components/user-profile/components/hair-concerns-form/hair-concerns-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hair-concerns-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './hair-concerns-form.component.html',
+  styleUrl: './hair-concerns-form.component.scss'
+})
+export class HairConcernsFormComponent {
+
+}
