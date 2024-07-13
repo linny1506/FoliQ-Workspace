80929c604aa31cc3e2b20fdbe873c505bc9f6b53
linnyHPx360 <linny1506@gmail.com>
2024-07-13 12:03:58 -0400

    Angular: Finished up features of curly-coily sliders

--- Angular-Testing/foliq-quiz/src/app/quiz/quiz-components/hair-texture/hair-texture-components/curly-coily/curly-coily.component.ts
+++ Angular-Testing/foliq-quiz/src/app/quiz/quiz-components/hair-texture/hair-texture-components/curly-coily/curly-coily.component.ts
@@ -12,6 +12,9 @@ import { MatSlider, MatSliderModule } from '@angular/material/slider';
   styleUrl: './curly-coily.component.scss'
 })
 export class CurlyCoilyComponent implements AfterViewInit {
+  // #region canvas things 
+
+
   // code based off of: https://vivekseth.com/parametric-drawing-canvas/
   canvasWidth:number = 300;
   canvasHeight:number = 100;
@@ -31,18 +34,66 @@ export class CurlyCoilyComponent implements AfterViewInit {
   bx:number = 2;   // pitch
   cx:number = 20;   // amplitude
 
-  
   draw() {
     this.context.moveTo(0, this.canvasHeight/2);    // bring the starting point of the drawing to the center left
     for(let t = 0; t <= this.tMax; t+=0.1) {
       // parametric function defined as such (plug this into desmos)
       // \left(a\left(t+b\sin(t)\right),\frac{c}{1+b}\left(1-b\cos(t)\right)\right)
       let x = this.ax * (t+this.bx * Math.sin(t));
-      if (this.canvasWidth < x ) break;       // stop the loop once the end of the canvas is reached
+      if (this.canvasWidth+this.cx < x ) break;       // stop the loop once the end of the canvas is reached. using this.cx as a buffer to finish out the last loop
       let y = this.canvasHeight/2.0 - (this.cx/(1+this.bx)) * (-this.bx * Math.cos(t));
       
       this.context.lineTo(x,y);
     }
     this.context.stroke();  // actually draw the lines
   }
+
+  clear() {
+    this.context.clearRect(0,0,this.canvasWidth,this.canvasHeight);
+    this.context.beginPath();
+  }
+
+  // #endregion 
+
+  // #region slider things 
+  // #region    period 
+  periodParams = {
+    min:0,
+    max:10,
+    step:0.1,
+  }
+  submitPeriod(event:any) {
+    this.ax = Number((event.target as HTMLInputElement).value);
+    this.clear();
+    this.draw();
+  }
+  // #endregion
+  // #region    pitch 
+  pitchParams = {
+    min:0,
+    max:15,
+    step:0.1,
+  }
+  submitPitch(event:any) {
+    this.bx = Number((event.target as HTMLInputElement).value);
+    this.clear();
+    this.draw();
+  }
+  // #endregion
+  // #region    amplitude 
+  amplitudeParams = {
+    min:0,
+    max:50,
+    step:1,
+  }
+  submitAmplitude(event:any) {
+    this.cx = Number((event.target as HTMLInputElement).value);
+    this.clear();
+    this.draw();
+  }
+  // #endregion
+
+  formatLabel(value:number) { return `${value}`; }
+
+  // #endregion
 }
